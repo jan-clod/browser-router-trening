db6132e1e43a1c407e6e387c1b908b97a2d2cac3
jan-clod <paviliong6tim@gmail.com>
2023-04-15 11:57:30 +0300

    routers

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,38 @@ import { Glavnaya } from './components/Glavnaya';
 import { Project } from './components/Project';
 import { Footer } from './components/Footer';
 import style from './App.module.css'
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className={style.App}>
       <div className={style.container}>
-          <div className={style.header}>
-              Header
-          </div>
+        <div className={style.header}>
+          Header
+        </div>
         <div className={style.res}>
           <div className={style.navbar}>
-            <Glavnaya/>
-            <Project/>
-            <Footer/>
+            <NavLink to={'/'} > Home </NavLink>
+            <NavLink to={'/Project'} > Project </NavLink>
+            <NavLink to={'/Footer'} > Footer </NavLink>
           </div>
           <div className={style.main}>
-           main
+            <Routes>
+              <Route path={'/'} element={<Glavnaya/>}/>
+              <Route path={'/Project'} element={<Project/>}/>
+              <Route path={'/Footer'} element={<Footer/>}/>
+            </Routes>
           </div>
         </div>
-   
-        </div>
 
-      
-      
-      
-     
-    
+      </div>
+
+
+
+
+
+
     </div>
   );
 }
