91728939588641ad583cd123188da0029bc1768c
Vasy <Vasya>
2023-04-15 11:44:24 +0300

    styleApp

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,33 @@ import './App.css';
 import { Glavnaya } from './components/Glavnaya';
 import { Project } from './components/Project';
 import { Footer } from './components/Footer';
+import style from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-     <p>Hellow yo yo yo </p>
-     <Glavnaya/>
-     <Project/>
-     <Footer/>
+    <div className={style.App}>
+      <div className={style.container}>
+          <div className={style.header}>
+              Header
+          </div>
+        <div className={style.res}>
+          <div className={style.navbar}>
+            <Glavnaya/>
+            <Project/>
+            <Footer/>
+          </div>
+          <div className={style.main}>
+           main
+          </div>
+        </div>
+   
+        </div>
+
+      
+      
+      
+     
+    
     </div>
   );
 }
